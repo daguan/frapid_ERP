4e8af168b58982ba00451735305acf8cecc369dd
binodnp@outlook.com <binodnp@outlook.com>
2016-10-14 02:37:31 +0545

    Rebundled core module database.

--- src/Frapid.Web/Areas/Frapid.Core/db/PostgreSQL/1.x/1.0/core-blank.sql
+++ src/Frapid.Web/Areas/Frapid.Core/db/PostgreSQL/1.x/1.0/core-blank.sql
@@ -920,6 +920,21 @@ END
 $$
 LANGUAGE plpgsql;
 
+-->-->-- src/Frapid.Web/Areas/Frapid.Core/db/PostgreSQL/1.x/1.0/src/06.functions-and-logic/core.get_currency_code_by_office_id.sql --<--<--
+DROP FUNCTION IF EXISTS core.get_currency_code_by_office_id(_office_id integer);
+
+CREATE FUNCTION core.get_currency_code_by_office_id(_office_id integer)
+RETURNS national character varying(50)
+AS
+$$
+BEGIN
+    RETURN currency_code 
+    FROM core.offices
+    WHERE office_id = _office_id;
+END
+$$
+LANGUAGE plpgsql;
+
 -->-->-- src/Frapid.Web/Areas/Frapid.Core/db/PostgreSQL/1.x/1.0/src/06.functions-and-logic/core.get_office_id_by_office_name.sql --<--<--
 DROP FUNCTION IF EXISTS core.get_office_id_by_office_name(_office_name text);
 
