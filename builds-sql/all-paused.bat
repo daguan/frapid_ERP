221f553e7d60b9966e55613364a64ae8009ffa26
binodnp@outlook.com <binodnp@outlook.com>
2017-01-10 01:49:54 +0545

    -Updated core postgresql schema -Update SQL Bundling script.

--- builds-sql/all-paused.bat
+++ builds-sql/all-paused.bat
@@ -1,3 +1,3 @@
 @echo off
-all.bat
+call all.bat
 pause
--- src/Frapid.Web/Areas/Frapid.Core/db/PostgreSQL/1.x/1.0/core-blank.sql
+++ src/Frapid.Web/Areas/Frapid.Core/db/PostgreSQL/1.x/1.0/core-blank.sql
@@ -1035,6 +1035,22 @@ END
 $$
 LANGUAGE plpgsql;
 
+-->-->-- src/Frapid.Web/Areas/Frapid.Core/db/PostgreSQL/1.x/1.0/src/06.functions-and-logic/core.get_hstore_field.sql --<--<--
+DROP FUNCTION IF EXISTS core.get_hstore_field(this public.hstore, _column_name text);
+
+CREATE FUNCTION core.get_hstore_field(_hstore public.hstore, _column_name text)
+RETURNS text
+AS
+$$
+   DECLARE _field_value text;
+BEGIN
+    _field_value := _hstore->_column_name;
+    RETURN _field_value;
+END
+$$
+LANGUAGE plpgsql;
+
+
 -->-->-- src/Frapid.Web/Areas/Frapid.Core/db/PostgreSQL/1.x/1.0/src/06.functions-and-logic/core.get_my_notifications.sql --<--<--
 DROP FUNCTION IF EXISTS core.get_my_notifications(_login_id bigint);
 
