72cb4d95a98a7c9d64d6b09f616520f5199f2c3c
binodnp@outlook.com <binodnp@outlook.com>
2017-01-30 15:13:09 +0545

    Added calendar module.

---
+++ src/Frapid.Web/Areas/Frapid.Calendar/Controllers/Backend/CalendarController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Frapid.Calendar.Controllers.Backend
+{
+    public sealed class CalendarController : CalendarBackendController
+    {
+        [Route("dashboard/calendar")]
+        public ActionResult Index()
+        {
+            return this.FrapidView(this.GetRazorView<AreaRegistration>("Backend/Index.cshtml", this.Tenant));
+        }
+    }
+}
