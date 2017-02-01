8323c00dc2a29723712484461d9cf360ac917483
binodnp@outlook.com <binodnp@outlook.com>
2017-02-02 02:08:10 +0545

    Update on Calendar Module.

---
+++ src/Frapid.Web/Areas/Frapid.Calendar/Configs.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using Frapid.ApplicationState.Cache;
+using Frapid.Configuration;
+
+namespace Frapid.Calendar
+{
+    public static class Configs
+    {
+        public static string GoogleApiKey => Get("GoogleApiKey");
+
+        public static string Get(string key)
+        {
+            string tenant = AppUsers.GetTenant();
+            return Get(tenant, key);
+        }
+
+        public static string Get(string tenant, string key)
+        {
+            string configurationFile = $"/Tenants/{tenant}/Configs/Calendar.config";
+            configurationFile = PathMapper.MapPath(configurationFile);
+
+            if (!File.Exists(configurationFile))
+            {
+                return string.Empty;
+            }
+
+            return ConfigurationManager.ReadConfigurationValue(configurationFile, key);
+        }
+    }
+}
