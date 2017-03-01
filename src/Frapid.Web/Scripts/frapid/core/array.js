297786e708d92ddafb8eafc4a73b8a911cc74c0d
binodnp@outlook.com <binodnp@outlook.com>
2017-03-01 23:34:16 +0545

    Removed obsolete git directories.

--- src/Frapid.Web/Scripts/frapid/core/array.js
+++
@@ -1,6 +0,0 @@
-﻿function chunk_array(arr, len) {
-    return arr.map(function (e, i) {
-        return i % len === 0 ? arr.slice(i, i + len) : null;
-    })
-        .filter(function (e) { return e; });
-};
--- src/Frapid.Web/Scripts/frapid/core/dom/popunder.js
+++
@@ -1,8 +0,0 @@
-function popUnder(div, button) {
-    div.removeClass("initially hidden");
-    div.fadeIn().position({
-        my: "left top",
-        at: "left bottom",
-        of: button
-    });
-};
